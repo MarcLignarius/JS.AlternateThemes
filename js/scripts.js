2b2e0ec074108b6fa12fecfc649587267fe6241b
Marc Davies <marcdaviesriot@gmail.com>
2020-01-10 16:06:57 -0800

    add script for light theme button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(function() {
-
+  $("button#light").click(function() {
+    $("body, h1, h4, p").removeClass();
+    $("body").addClass("light-background");
+    $("h1, h4, p").addClass("light-text");
+  });
 });
