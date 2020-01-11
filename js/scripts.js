acfffac09d7589a45166d671e7b68148ce75482d
Marc Davies <marcdaviesriot@gmail.com>
2020-01-10 16:07:14 -0800

    add script for dark theme button

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,9 @@ $(function() {
     $("body").addClass("light-background");
     $("h1, h4, p").addClass("light-text");
   });
+  $("button#dark").click(function() {
+    $("body, h1, h4, p").removeClass();
+    $("body").addClass("dark-background");
+    $("h1, h4, p").addClass("dark-text");
+  });
 });
